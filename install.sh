2a83b3fed73131d37664a418b7cb69e19254ad14
Cloud User <centos@centos-noud-4vcpu-50gb.novalocal>
2021-06-28 16:02:33 +0000

    Added all-in-one shell script

--- install.sh
+++ install.sh
@@ -1,10 +1,32 @@
 #! /usr/bin/bash
 
-if uname -a | grep 'Ubuntu'
+set -e
+clean_up () {
+    ARG=$?
+    echo "> clean_up"
+    echo "rm -rf ${DL}"
+    exit $ARG
+} 
+trap clean_up EXIT
+
+source /etc/os-release
+
+if [ "${ID_LIKE}" == 'debian' ]
 then
-	sudo apt install build-essential
+        sudo apt update
+	sudo DEBIAN_FRONTEND=noninteractive apt install -q -y build-essential wget
+elif [ "${ID_LIKE}" == 'rhel fedora' ]
+then
+	sudo yum -q -y groupinstall 'Development Tools'
+	sudo yum -q -y install wget kernel-devel kernel-headers
 else
-	sudo yum groupinstall 'Development Tools'
+	echo "Unsupported distro ${ID_LIKE}. See README.md for manual installation."
 fi
 
-make
+DL=$(mktemp -d)
+
+wget -P ${DL} https://core.fuga.cloud:8080/swift/v1/faba49e2b80249c68ebe11119c56b69a/nvidia/NVIDIA-Linux-x86_64-460.73.01-grid.run
+wget -P ${DL} https://developer.download.nvidia.com/compute/cuda/11.2.2/local_installers/cuda_11.2.2_460.32.03_linux.run
+
+sudo bash ${DL}/NVIDIA-Linux-x86_64-460.73.01-grid.run -s
+sudo bash ${DL}/cuda_11.2.2_460.32.03_linux.run --silent --samples --toolkit
